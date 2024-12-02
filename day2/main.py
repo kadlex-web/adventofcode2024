d0ecabfa1853654f4dd95f72f99549944a32ae1e
BenE <158221658+kadlex-web@users.noreply.github.com>
2024-12-02 17:08:32 +0000

    created loop for difference

--- day2/main.py
+++ day2/main.py
@@ -1,3 +1,14 @@
-with open("adventofcodeday2.txt") as file:
-    file_contents = file.read()
-print(file_contents)
+def difference(a, b):
+    return abs(a - b)
+
+file = "test.txt"
+
+with open(file) as f:
+    file_contents = f.read()
+
+for line in file_contents.split("\n"):
+    i = 1
+    while i < len(line.split()):
+        diff = difference(int(line.split()[i-1]), int(line.split()[i]))
+        i += 1
+        print(diff)
