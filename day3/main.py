abe2c96148294c490da858e8f8104b69e130f84b
BenE <158221658+kadlex-web@users.noreply.github.com>
2024-12-03 18:41:23 +0000

    day 3 solved

--- day3/main.py
+++ day3/main.py
@@ -6,7 +6,22 @@ def find_all(str):
     expression2 = "don't\(\)|do\(\)|mul\([0-9]+,[0-9]+\)"
     found_sequences = re.findall(expression2, str)
     return found_sequences
-    
+
+
+def do_dont_replace(list):
+    new_list = []
+    for str in list:
+        if str == "do()":
+            new_str = str.replace("do()", "on")
+            new_list.append(new_str)
+        elif str == "don't()":
+            new_str = str.replace("don't()", "off")
+            new_list.append(new_str)
+        else:
+            new_list.append(str)
+    return new_list
+
+
 def trim(list):
     trimmed_list = []
     for str in list:
@@ -20,6 +35,20 @@ def trim(list):
 def multiply(str):
     values = str.split("*")
     return int(values[0]) * int(values[1])
+
+
+def mega_multiply(list):
+    do_state = True
+    total = 0
+    for expression in list:
+        if expression == "on":
+            do_state = True
+        elif expression == "off":
+            do_state = False
+        else:
+            if do_state:
+                total += multiply(expression)
+    return total
 
 
 file = "day3.txt"
@@ -36,7 +65,13 @@ def main():
     return total
 
 
+# call print(main2()) for solution
+
+
 def main2():
-    matches = find_all(file_contents)
-    print(matches)
+    matches = do_dont_replace(find_all(file_contents))
+    trimmed_matches = trim(matches)
+    print(mega_multiply(trimmed_matches))
+
+
 main2()
