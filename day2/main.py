2e5a126a84083fa1a5309f56898c15566ce1ba39
BenE <158221658+kadlex-web@users.noreply.github.com>
2024-12-03 21:59:47 +0000

    trying a branch commit

--- day2/main.py
+++ day2/main.py
@@ -1,110 +1,52 @@
-def difference(a, b):
-    return int(a) - int(b)
-
-file = "adventofcodeday2.txt"
-file2 = "danger.txt"
-
-#Initial Input
-with open(file) as f:
-    file_contents = f.read()
-
-total_safe = 0
-safe_list = []
-danger_list = []
-
-for line in file_contents.split("\n"):
-    my_list = line.split()
-    danger_count = 0
-
-    if int(my_list[0]) > int(my_list[-1]):
-        #print("descending")
-        for i in range(1, len(my_list)):
-            diff = difference(my_list[i], my_list[i - 1])
-            if diff < -3 or diff > -1:
-                #print("unsafe")
-                danger_count += 1
-            elif diff > 0:
-                print("unsafe")
-                danger_count += 1
-
-    elif int(my_list[0]) < int(my_list[-1]):
-        #print("ascending")
-        for i in range(1, len(my_list)):
-            diff = difference(my_list[i], my_list[i - 1])
-            if diff > 3 or diff < 1:
-                #print("unsafe")
-                danger_count += 1
-            elif diff < 0:
-                print("unsafe")
-                danger_count += 1
-                
-    elif int(my_list[0]) == int(my_list[-1]):
-        danger_count += 1
+def string_to_int_list(list):
+    converted_list = []
+    int_list = []
+    for str in list:
+        converted_list.append(str.split(" "))
         
-    if danger_count == 0:
-        #print("safe levels")
-        safe_list.append(my_list)
-        total_safe += 1
-        
-    if danger_count == 1:
-        danger_list.append(my_list)
+    for list in converted_list:
+        temp_list = []
+        for str in list:
+            temp_list.append(int(str))
+        int_list.append(temp_list)    
+    return int_list
 
-#Begin Part II
-recovered_list = []
-final_list = []
-temp_list = []
-
-for line in danger_list:
-    temp_list.append(line)
-    if int(line[0]) > int(line[-1]):
-        print("descending")
-        for i in range(1, len(line)):
-            diff = difference(line[i], line[i - 1])
-
-            if diff < -3 or diff > -1:
-                print(f"Unsafe levels found between index {i-1} and {i}")
-                print(f"{i+1}      {len(line)}")
-                if (i + 1) == len(line):
-                    print("I think we can fix that. The bad level is last in the sequence.\n")
-                    recovered_list.append(line)
-                    break
-
-                level_check = abs(difference(line[i-1], line[i+1]))
-                if level_check <= 3 and level_check >= 1:
-                    print(f"I think we can fix that. Showing a difference of {level_check}\n")
-                    recovered_list.append(line)
-                else:
-                    print("Think it's too much -- this one is unsafe\n")
-                    final_list.append(line)
-                 
-    elif int(line[0]) < int(line[-1]):
-        print("ascending")
-        for i in range(1, len(line)):
-            diff = difference(line[i], line[i - 1])
-
-            if diff > 3 or diff < 1:
-                print(f"Unsafe levels found between index {i-1} and {i}")
-                print(f"{i + 1}      {len(line)}")
-                if (i + 1) == len(line):
-                    print("I think we can fix that. The bad level is last in the sequence.\n")
-                    recovered_list.append(line)
-                    break
-                level_check = abs(difference(line[i-1], line[i+1]))
-                if level_check <= 3 and level_check >= 1:
-                    print(f"I think we can fix that. Showing a difference of {level_check}\n")
-                    recovered_list.append(line)
-                else:
-                    print("Think it's too much -- this one is unsafe\n")
-                    final_list.append(line)
+def safety_check(a,b):
+    expression = abs(a-b)
+    print(expression)
+    if (expression <= 3) and (expression >= 1):
+        print("true")
+        return True
     else:
-        print("I think the lines are equal")
-        final_list.append(line)
+        print("false")
+        return False
 
-print(len(temp_list))
-print(len(safe_list))
-print(len(danger_list))
-print(len(recovered_list))
-print(len(final_list))
+def check_levels(list):
+    if len(list) == 2:
+        return True
+    if safety_check(list[0], list[1]):
+        check_levels(list[1:])
+    else:
+        return False
+        
+with open("test.txt") as f:
+    file_contents = f.read()
+    f.close()
 
-for list in final_list:
-    print(list)
+def main():
+    safe_list = []
+    unsafe_list = []
+    data = string_to_int_list(file_contents.split("\n"))
+    
+    for list in data:
+        print(list)
+        print(check_levels(list))
+        if check_levels(list):
+            safe_list.append(list)
+        else:
+            unsafe_list.append(list)
+            
+    #print(len(safe_list))
+    #print(len(unsafe_list))
+    
+main()
