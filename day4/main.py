3a32818c8653a54584d7882bfca71cdf5e0dbd6d
BenE <158221658+kadlex-web@users.noreply.github.com>
2024-12-04 21:52:16 +0000

    end of day4 commit

--- day4/main.py
+++ day4/main.py
@@ -10,14 +10,18 @@ def find_m(x, y, new_char, new_data):
         checked_coords_x = [
             [x, y-1],[x, y+1],[x+1, y-1],[x+1, y],[x+1, y+1]
         ]
+        return checked_coords_x
     elif y == 0:
         checked_coords_y = [
             [x-1, y], [x+1, y],[x-1, y+1],[x, y+1],[x+1, y+1]
         ]
+        return checked_coords_y
     else:
         checked_coords_any = [
             [x-1, y-1], [x, y-1], [x+1, y-1], [x-1, y], [x+1, y], [x-1, y+1], [x, y+1], [x+1, y+1]
         ]
+        return checked_coords_any
+        
 with open("sample.txt") as f:
     data = f.read().split("\n")
     f.close()
@@ -31,12 +35,14 @@ for list in data:
 
 def main():
     list_of_x = []
+    values = []
     for x in range(0, len(new_data)):
         for y in range(0, len(new_data[x])):
             if find_x(x, y, new_data):
                 list_of_x.append([x, y])
                 
     for coords in list_of_x:
-        find_m(coords[0], coords[1], "M", new_data)
-
+        values += find_m(coords[0], coords[1], "M", new_data)
+        
+    return values
 print(main())
