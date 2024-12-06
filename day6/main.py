290eb4abc26d146b9bd71d12f5775c61a814b2b7
kadlex <akulp18@gmail.com>
2024-12-06 18:51:02 -0500

    it wasnt recursion lol -- just a good ol while loop

--- day6/main.py
+++ day6/main.py
@@ -6,47 +6,48 @@ def find_start(data):
             
 def patrol(x, y, map, direction):
     if y == 0 and direction == "left":
-        print("guard is about to leave the map")
+        print("guard is about to leave the map on the left")
         map[x][y] = "X"
         return map
-    if x == 0 and direction == "up":
-        print("guard is about to leave the map")
+    elif x == 0 and direction == "up":
+        print("guard is about to leave the map upward")
         map[x][y] = "X"
         return map      
-    if x == len(map) - 1 and direction == "down":
-        print("guard is about to leave the map")
+    elif x == len(map) - 1 and direction == "down":
+        print("guard is about to leave the map downward")
         map[x][y] = "X"
         return map
-    if y == len(map) - 1 and direction == "right":
-        print("guard is about to leave the map")
+    elif y == len(map) - 1 and direction == "right":
+        print("guard is about to leave the map to the right")
         map[x][y] = "X"
         return map
-    
-    map[x][y] = "X"
-    if direction == "up":
-        next_square = map[x-1][y]
-        if next_square == "X" or next_square == ".":
-            return patrol(x-1, y, map, direction)
-        if next_square == "#":
-            return patrol(x, y, map, "right")
-    elif direction == "right":
-        next_square = map[x][y+1]
-        if next_square == "X" or next_square == ".":
-            return patrol(x, y+1, map, direction)
-        if next_square == "#":
-            return patrol(x, y, map, "down")
-    elif direction == "down":
-        next_square = map[x+1][y]
-        if next_square == "X" or next_square == ".":
-            return patrol(x+1, y, map, direction)
-        if next_square == "#":
-            return patrol(x, y, map, "left")
-    elif direction == "left":
-        next_square = map[x][y-1]
-        if next_square == "X" or next_square == ".":
-            return patrol(x, y-1, map, direction)
-        if next_square == "#":
-            return patrol(x, y, map, "up")    
+
+    else:
+        map[x][y] = "X"
+        if direction == "up":
+            next_square = map[x-1][y]
+            if next_square == "X" or next_square == ".":
+                return patrol(x-1, y, map, direction)
+            elif next_square == "#":
+                return patrol(x, y, map, "right")
+        elif direction == "right":
+            next_square = map[x][y+1]
+            if next_square == "X" or next_square == ".":
+                return patrol(x, y+1, map, direction)
+            elif next_square == "#":
+                return patrol(x, y, map, "down")
+        elif direction == "down":
+            next_square = map[x+1][y]
+            if next_square == "X" or next_square == ".":
+                return patrol(x+1, y, map, direction)
+            elif next_square == "#":
+                return patrol(x, y, map, "left")
+        elif direction == "left":
+            next_square = map[x][y-1]
+            if next_square == "X" or next_square == ".":
+                return patrol(x, y-1, map, direction)
+            elif next_square == "#":
+                return patrol(x, y, map, "up")    
 
 def total_squares(map):
     total_x = 0
@@ -64,15 +65,55 @@ with open ("day6.txt") as f:
         for string in list:
             new_str = string.strip()
             temp_list.append(new_str)
-        print(temp_list)
         map.append(temp_list)
     f.close()
 
 def main():
-    start_x, start_y = find_start(map)
-    print(start_x)
-    print(start_y)
-    #final_map = patrol(start_x, start_y, map, "up")
-    #print(total_squares(final_map))
-
-main()
+    x, y = find_start(map)
+    direction = "up"
+    moving = True
+    while moving:
+        if y == 0 and direction == "left":
+            print("guard is about to leave the map on the left")
+            map[x][y] = "X"
+            moving = False
+        elif x == 0 and direction == "up":
+            print("guard is about to leave the map upward")
+            map[x][y] = "X"
+            moving = False   
+        elif x == len(map) - 1 and direction == "down":
+            print("guard is about to leave the map downward")
+            map[x][y] = "X"
+            moving = False
+        elif y == len(map) - 1 and direction == "right":
+            print("guard is about to leave the map to the right")
+            map[x][y] = "X"
+            moving = False
+        else:
+            map[x][y] = "X"
+            if direction == "up":
+                next_square = map[x-1][y]
+                if next_square == "X" or next_square == ".":
+                    x = x-1
+                elif next_square == "#":
+                    direction = "right"  
+            elif direction == "right":
+                next_square = map[x][y+1]
+                if next_square == "X" or next_square == ".":
+                    y = y + 1
+                elif next_square == "#":
+                    direction = "down"
+            elif direction == "down":
+                next_square = map[x+1][y]
+                if next_square == "X" or next_square == ".":
+                    x = x + 1
+                elif next_square == "#":
+                    direction = "left"
+            elif direction == "left":
+                next_square = map[x][y-1]
+                if next_square == "X" or next_square == ".":
+                    y = y - 1
+                elif next_square == "#":
+                    direction = "up"
+    return total_squares(map)
+print(main())
