861776a45d818756c98deee4ea9aef26da855311
kadlex <akulp18@gmail.com>
2024-12-18 22:08:14 -0500

    day 4 solution -- really lazy logic but it was the only way it made sense in my brain

--- day4/day4.py
+++ day4/day4.py
@@ -37,34 +37,89 @@ def check_string(x, y, hash_table):
                             if str3 == "XMAS":
                                 count += 1
     return count
+
+def check_crossmas(x, y, hash_table):
+    def get_corners_dir(x,y):
+        corner_coordinates = [(-1,-1), (-1, 1), (1, -1), (1,1)]
+        corner_dir = {}
+        directions = ["NW", "NE", "SW", "SE"]
+        corners = list(map(lambda n: (n[0] + x, n[1] + y), corner_coordinates))
+
+        for i in range(0, len(directions)):
+            corner_dir[directions[i]] = hash_table[corners[i]]
+        return corner_dir
     
+    if x < 1 or y < 1:
+        return 0
+    if x > 138 or y > 138:
+        return 0
+    
+    corner_dict = get_corners_dir(x,y)
+    if list(corner_dict.values()).count("M") == 2:
+        if corner_dict["NW"] == "M" and corner_dict["SW"] == "M":
+            if corner_dict["NE"] == "S" and corner_dict["SE"] == "S":
+                return 1
+            else:
+                return 0
+        elif corner_dict["NE"] == "M" and corner_dict["NW"] == "M":
+            if corner_dict["SE"] == "S" and corner_dict["SW"] == "S":
+                return 1
+            else:
+                return 0
+        elif corner_dict["NE"] == "M" and corner_dict["SE"] == "M":
+            if corner_dict["NW"] == "S" and corner_dict["SW"] == "S":
+                return 1
+            else:
+                return 0
+        elif corner_dict["SE"] == "M" and corner_dict["SW"] == "M":
+            if corner_dict["NE"] == "S" and corner_dict["NW"] == "S":
+                return 1
+            else:
+                return 0
+        else:
+            return 0
+    else:
+        return 0
+
+
+
 with open("data.txt") as f:
     data = f.read().split("\n")
     f.close()
 
 new_data = []
-for list in data:
+for lst in data:
     temp_list = []
-    for char in list:
+    for char in lst:
         temp_list.append(char)
     new_data.append(temp_list)
 
 sample_data = []
-for list in new_data:
+for lst in new_data:
     temp_list = []
-    for value in list:
+    for value in lst:
         temp_list.append(value)
     sample_data.append(temp_list)
 
 if __name__ == "__main__":
     table = get_hash_table(sample_data)
     x_list = []
-    
+    a_list = []
+
     for key,value in table:
         if table[(key, value)] == "X":
             x_list.append((key, value))
 
+    for key,value in table:
+        if table[(key, value)] == "A":
+            a_list.append((key, value))
+
     xmas_count = 0
     for value in x_list:
         xmas_count += check_string(value[0], value[1], table)
-    print(xmas_count)
+    #print(xmas_count)
+
+    crossmas_count = 0
+    for value in a_list:
+        crossmas_count += check_crossmas(value[0], value[1], table)
+    print(crossmas_count)
